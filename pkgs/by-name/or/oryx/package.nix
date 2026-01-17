8ba3bc1fee93f4eb95809795a1e7f4c8b8b7f928
Aali Ansari <sigmatotal1@gmail.com>
2026-01-16 21:45:30 -0600

    fix: enable unstable features

--- pkgs/by-name/or/oryx/package.nix
+++ pkgs/by-name/or/oryx/package.nix
@@ -16,6 +16,8 @@ rustPlatform.buildRustPackage rec {
   };
 
   cargoHash = "sha256-u4Hu924dEBGzSZ2ywISxzuh4PK5LX8O/mEFgMBtYnf0=";
+  # Oryx depends on trivial_bounds unstable feature
+  env.RUSTC_BOOTSTRAP = 1;
 
   meta = {
     description = "Interactive find and replace in the terminal";
